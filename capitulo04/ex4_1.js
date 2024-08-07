bcd7eccd0403aabb62f7a0be5cd95c8a77ab78cf
emanuel <emanulpereira45@gmail.com>
2024-08-07 16:26:19 -0300

    Adicionei observações sobre o uso do required, step, min e max no html. E no JS referência a id e uso de String Template.

--- capitulo04/ex4_1.js
+++ capitulo04/ex4_1.js
@@ -1,6 +1,6 @@
-let frm = document.querySelector('form');
-let resp01 = document.querySelector('#outNota');
-let resp02 = document.querySelector('#outResultado');
+let frm = document.querySelector('form');//Referência ao formulário do HTML;
+let resp01 = document.querySelector('#outNota');//Referência ao ID do h3. Id sempre usar o # pra referênciar.
+let resp02 = document.querySelector('#outResultado');//Referência ao ID do h3
 
 frm.addEventListener("submit", (e) =>{
     //Convertendo a entrada em Number//
@@ -9,7 +9,7 @@ frm.addEventListener("submit", (e) =>{
     let notaDois = Number(frm.inNotaDois.value);
 
     let media_final = (notaUm+notaDois)/2
-    resp01.innerText = `Média das Notas: ${media_final.toFixed(2)}`;
+    resp01.innerText = `Média das Notas: ${media_final.toFixed(2)}`;//Uso de String Template.
 
     //Valida a Situação do Aluno de Acordo com sua média final:
     if(media_final>=7){
