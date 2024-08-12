72fc1447906299c01964c607261c5d0093956a92
emanuel <emanulpereira45@gmail.com>
2024-08-12 20:42:50 -0300

    Adicionei um comentário sobre o uso do .checked e a declação da variável pesoIdeal.

--- capitulo04/ex4_2.js
+++ capitulo04/ex4_2.js
@@ -4,8 +4,10 @@ let saida_01 = document.querySelector('h3');
 frm.addEventListener('submit', (e) =>{
     let nome = frm.inNome.value;
     let altura = Number(frm.inAltura.value);
-    let rdMasculino = frm.inRadioMasculino.checked;
-    let pesoIdeal;
+    let rdMasculino = frm.inRadioMasculino.checked;/*Como usei no HTML um input do tipo radio, 
+    para checar se ele foi marcado, ou seja, se o seu valor é true, usei o .checked. Com isso, 
+    estou avaliando se o botão com id: inRadioMasculino está marcado. */
+    let pesoIdeal; //Declarei a variável com let, pois seu valor pode ser reatribuido, conforme execução do código.
 
     if(rdMasculino){
         pesoIdeal = 22*Math.pow(altura,2);
